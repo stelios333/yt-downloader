985cf27495292b8b62e90bb32fafb5f09c148c16
stelios333 <63864544+stelios333@users.noreply.github.com>
2021-06-24 11:03:15 +0300

    Update homepage

--- index.php
+++ index.php
@@ -7,6 +7,8 @@ header("Access-Control-Allow-Origin: *")
 <head>
     <title> Youtube downloader </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <link rel="icon" href="./favicon.png" type="image/png">
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
